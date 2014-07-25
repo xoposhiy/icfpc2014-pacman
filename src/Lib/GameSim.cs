980d1ef2ad950e1571e1c6c146e89ea9859c39a7
Pavel Egorov <xoposhiy@bk.ru>
2014-07-25 17:35:36 +0200

    Game simulator interfaces

---
+++ src/Lib/GameSim.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace Lib
+{
+	class GameSim
+	{
+		public MapCell[,] Map { get; set; }
+		public Func<LValue, World, Tuple<LValue, Direction>> Step { get; set; }
+		public LValue InitialState { get; set; }
+
+		public GameSim(MapCell[,] map, Func<LValue, World, Tuple<LValue, Direction>> step, LValue initialState)
+		{
+			Map = map;
+			Step = step;
+			InitialState = initialState;
+		}
+
+		public void Tick()
+		{
+			//ToDo
+		}
+	}
+
+	public class World
+	{
+		public World(MapCell[,] map, LManState lMan, List<GhostState> ghosts, int fruitTicksRemaining)
+		{
+			Map = map;
+			LMan = lMan;
+			Ghosts = ghosts;
+			FruitTicksRemaining = fruitTicksRemaining;
+		}
+
+		public readonly MapCell[,] Map;
+		public readonly LManState LMan;
+		public readonly List<GhostState> Ghosts;
+		public readonly int FruitTicksRemaining;
+	}
+	
+	public enum MapCell
+	{
+		Wall = 0,
+		Empty,
+		Pill,
+		PowerPill,
+		Fruit,
+		LManStartLoc,
+		GhostStartLoc
+	}
+
+
+
+	public class GhostState
+	{
+		public GhostState(GhostVitality vitality, Point location, Direction direction)
+		{
+			Vitality = vitality;
+			Location = location;
+			Direction = direction;
+		}
+
+		public readonly GhostVitality Vitality;
+		public readonly Point Location;
+		public readonly Direction Direction;
+	}
+
+	public enum GhostVitality
+	{
+		Standard = 0,
+		Fright,
+		Invisible
+	}
+
+	public class LManState
+	{
+		public LManState(int powerPillRemainingTicks, Point location, Direction direction, int lives, int score)
+		{
+			PowerPillRemainingTicks = powerPillRemainingTicks;
+			Location = location;
+			Direction = direction;
+			Lives = lives;
+			Score = score;
+		}
+
+		///<summary>Lambda man vitality</summary>
+		public readonly int PowerPillRemainingTicks;
+
+		public readonly Point Location;
+
+		public readonly Direction Direction;
+
+		public readonly int Lives;
+
+		public readonly int Score;
+	}
+
+	public enum Direction
+	{
+		Up = 0,
+		Right,
+		Down,
+		Left
+	}
+}
---
+++ src/Lib/LValue.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lib
+{
+	public class LValue
+	{
+		public static LValue FromInt(int value)
+		{
+			return new LValue(LTag.Int, value);
+		}
+		public static LValue FromClosure(int address)
+		{
+			return new LValue(LTag.Closure, address);
+		}
+		public static LValue FromPair(LValue head, LValue tail)
+		{
+			return new LValue(LTag.Pair, 0, head, tail);
+		}
+
+		public LValue(LTag tag, int value, LValue head = null, LValue tail = null)
+		{
+			Tag = tag;
+			Value = value;
+			Head = head;
+			Tail = tail;
+		}
+
+		public readonly LTag Tag;
+		public readonly int Value;
+		public readonly LValue Head;
+		public readonly LValue Tail;
+
+	}
+
+	public enum LTag
+	{
+		Int,
+		Pair,
+		Closure
+	}
+
+
+	public static class LValueExtensions
+	{
+
+		public static bool IsInt(this LValue v)
+		{
+			return v.Tag == LTag.Int;
+		}
+
+		public static IEnumerable<LValue> AsEnumerable(this LValue v)
+		{
+			while (!v.IsInt() || v.Value != 0)
+			{
+				if (v.Tag != LTag.Pair) throw new Exception(v.ToString());
+				yield return v.Head;
+				v = v.Tail;
+			}
+		}
+
+		public static List<T> AsList<T>(this LValue list, Func<LValue, T> parseElement)
+		{
+			throw new NotImplementedException();
+		}
+
+		public static char[,] AsMap(this LValue map)
+		{
+			throw new NotImplementedException();
+		}
+
+		//... Other extension methods to interpret LValues
+
+	}
+
+}
