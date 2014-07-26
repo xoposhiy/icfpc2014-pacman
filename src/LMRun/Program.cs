0c0eb70dd48e6f2d4398adb6726b075ee1716d7d
spaceorc <spaceorc@skbkontur.ru>
2014-07-26 23:27:15 +0600

    use world in debugger

--- src/LMRun/Program.cs
+++ src/LMRun/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using Lib;
+using Lib.Game;
 using Lib.LispLang;
 using Lib.LMachine;
 using Lib.LMachine.Intructions;
@@ -15,14 +16,19 @@ namespace LMRun
 	{
 		private static void Main(string[] args)
 		{
-			Run("generated");
+			Run("generated", "maze1.txt");
 		}
 
-		private static void Run(string name)
+		private static void Run(string programName, string worldName)
 		{
-			var p = File.ReadAllText(KnownPlace.GccSamples + name + ".mgcc");
+			var p = File.ReadAllText(KnownPlace.GccSamples + programName + ".mgcc");
 			var prog = LParser.Parse(p);
+			var world = new World(MapUtils.LoadFromKnownLocation(worldName)).ToLValue();
 			var m = new LMachineInterpreter(prog.Program);
+			m.State.DataStack.Push(world);
+			m.State.DataStack.Push(42);
+			m.State.DataStack.Push(LValue.FromClosure(0, null));
+			new Tap(2).Execute(m.State);
 			while (!m.State.Stopped)
 			{
 				ShowState(m, prog);
