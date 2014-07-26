d39543cdca20dd28c002259558fc650b08677855
andrew <Andrew.Kostousov@gmail.com>
2014-07-26 12:36:31 +0600

    + Instrunction[].ToGcc()

---
+++ src/Lib/LMachine/Intructions/InstructionExtensions.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Lib.LMachine.Intructions
+{
+	public static class InstructionExtensions
+	{
+		[NotNull]
+		public static string ToGcc([NotNull] this IEnumerable<Instruction> program)
+		{
+			return string.Join("\r\n", program.Select(x => ToGcc((Instruction)x)));
+		}
+
+		[NotNull]
+		public static string ToGcc([NotNull] this Instruction instruction)
+		{
+			var args = string.Empty;
+			switch (instruction.Type)
+			{
+				case InstructionType.Ldc:
+					args = ((Ldc)instruction).Value.ToString();
+					break;
+				case InstructionType.Ld:
+					var ld = ((Ld)instruction);
+					args = ld.FrameIndex + " " + ld.ValueIndex;
+					break;
+				case InstructionType.Sel:
+					var sel = ((Sel)instruction);
+					args = sel.TrueAddress + " " + sel.FalseAddress;
+					break;
+				case InstructionType.Ldf:
+					args = ((Ldf)instruction).Address.ToString();
+					break;
+				case InstructionType.Ap:
+					args = ((Ap)instruction).FrameSize.ToString();
+					break;
+				case InstructionType.Dum:
+					args = ((Dum)instruction).FrameSize.ToString();
+					break;
+				case InstructionType.Rap:
+					args = ((Rap)instruction).FrameSize.ToString();
+					break;
+				case InstructionType.Tap:
+					args = ((Tap)instruction).FrameSize.ToString();
+					break;
+				case InstructionType.Trap:
+					args = ((Trap)instruction).FrameSize.ToString();
+					break;
+				case InstructionType.Tsel:
+					var tsel = ((Tsel)instruction);
+					args = tsel.TrueAddress + " " + tsel.FalseAddress;
+					break;
+				case InstructionType.St:
+					var st = ((St)instruction);
+					args = st.FrameIndex + " " + st.ValueIndex;
+					break;
+			}
+			return string.Format("{0} {1}", instruction.Type.ToString().ToUpper(), args);
+		}
+	}
+}
---
+++ src/Lib/LMachine/Intructions/InstructionExtensions_Test.cs
@@ -0,0 +1,34 @@
+﻿using Lib.LMachine.Parsing;
+using NUnit.Framework;
+
+namespace Lib.LMachine.Intructions
+{
+	[TestFixture]
+	public class InstructionExtensions_Test
+	{
+		[Test]
+		public void ToGcc()
+		{
+			const string source = @"half = 21
+
+LDC  half
+LDF  doubler    ; load body
+AP   1    ; call body with 1 variable in a new frame
+RTN
+
+doubler:
+LD   0 0  ; var x    :body
+LD   0 0  ; var x
+ADD
+RTN";
+			Assert.That(LParser.Parse(source).Program.ToGcc(), Is.EqualTo(@"LDC 21
+LDF 4
+AP 1
+RTN 
+LD 0 0
+LD 0 0
+ADD 
+RTN "));
+		}
+	}
+}
